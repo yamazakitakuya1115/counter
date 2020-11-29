bfd769d59956d283206f4768ccb45e415d9f9de8
yamazakitakuya <71809256+yamazakitakuya1115@users.noreply.github.com>
2020-11-29 21:30:08 +0900

    カウント機能

--- app/controllers/counts_controller.rb
+++ app/controllers/counts_controller.rb
@@ -1,5 +1,16 @@
 class CountsController < ApplicationController
   def index
     @count = Count.all
+    @counter = @count.length
+  end
+
+  def create
+    Count.create(count_params)
+    redirect_to root_path
+  end
+
+  private
+  def count_params
+    params.permit()
   end
 end
