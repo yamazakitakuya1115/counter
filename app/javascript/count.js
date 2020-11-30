036a8443d55e68e90ec3d39181caead65e93e734
yamazakitakuya <71809256+yamazakitakuya1115@users.noreply.github.com>
2020-11-30 17:34:37 +0900

    Update count.js

---
+++ app/javascript/count.js
@@ -0,0 +1,8 @@
+function check() {
+  const number = document.getElementsByClassName('count-number');
+  const btn = document.getElementById('count-btn');
+    btn.addEventListener("click", () => {
+      console.log(12345);
+  });
+}
+window.addEventListener("load", check)
